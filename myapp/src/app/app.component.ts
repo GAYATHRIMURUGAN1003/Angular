7872c59d6b77babc38465b32f111e7980cca2a48
GAYATHRIMURUGAN1003 <gaya3murugan1003@gmail.com>
2024-05-08 20:02:57 +0530

    save

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  firstname: string="Gayathri"
+  lastname:string="Murugan"
+  SECONDPERSON: string="sivakamasundari"
+  brother:string="venkatesh"
+  age:string="24"
+  designation:string="analyst"
+
+  y=2;
+  z=3;
+  x=4;
+  msg:string="hai";
+  myEvent(){
+    this.msg="bye";
+  }
 }
--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -3,32 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { ArticleComponent } from './article/article.component';
-import { FooterComponent } from './footer/footer.component';
-import { DeptComponent } from './dept/dept.component';
-import { LibComponent } from './lib/lib.component';
-import { SportsComponent } from './sports/sports.component';
-import { EceComponent } from './dept/ece/ece.component';
-import { MechComponent } from './dept/mech/mech.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    ArticleComponent,
-    FooterComponent,
-    DeptComponent,
-    LibComponent,
-    SportsComponent,
-    EceComponent,
-    MechComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
