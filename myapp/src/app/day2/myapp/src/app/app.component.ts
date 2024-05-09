dadbad44960548fa55ecbbd9aad7a603226b8645
Gayathri Murugan <gayathri.m@codeboardtech.com>
2024-05-09 19:17:17 +0530

    commited

---
+++ myapp/src/app/day2/myapp/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'myapp';
+  firstname: string="Gayathri"
+  lastname:string="Murugan"
+  SECONDPERSON: string="sivakamasundari"
+  brother:string="venkatesh"
+  age:string="24"
+  designation:string="analyst"
+
+  y=2;
+  z=3;
+  x=4;
+  msg:string="";
+  myEvent(m:any){
+    console.log(m);
+    this.msg=m.target.value;
+  }
+}
